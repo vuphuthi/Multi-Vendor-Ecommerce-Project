b756036eb4c4517ae40897751eb1eb601cbafed1
vuphuthi <duxinhtrai754@gmail.com>
2024-03-18 18:17:38 +0700

    doi mat khau vendor

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -53,4 +53,30 @@ class VendorController extends Controller
         );
         return redirect()->back()->with($notification);
     }
+    public function VendorChangePassword(){
+        return view('vendor.vendor_change_password');
+    }
+    public function VendorUpdatePassword(Request $request){
+        // validate
+        $request->validate([
+            'old_password' => 'required',
+            'new_password' => 'required|confirmed',
+            'new_password_confirmation' => 'required',
+        ], [
+            'old_password.required' => 'Vui lòng nhập mật khẩu cũ.',
+            'new_password.required' => 'Vui lòng nhập mật khẩu mới.',
+            'new_password.confirmed' => 'Mật khẩu mới không khớp với xác nhận mật khẩu.',
+            'new_password_confirmation.required' => 'Vui lòng xác nhận lại mật khẩu mới.',
+
+        ]);
+        // Khớp với mật khẩu cũ
+        if(!Hash::check($request->old_password,auth::user()->password)){
+            return back()->with('error','Mật khẩu cũ không khớp!');
+        }
+        // cập nhật password
+        User::whereId(auth()->user()->id)->update([
+            'password' => Hash::make($request->new_password)
+        ]);
+        return back()->with('status',"Thay đổi mật khẩu thành công");
+    }
 }
--- resources/views/vendor/body/sidebar.blade.php
+++ resources/views/vendor/body/sidebar.blade.php
@@ -12,7 +12,7 @@
     <!--navigation-->
     <ul class="metismenu" id="menu">
         <li>
-            <a href="{{route('admin.dashboard')}}">
+            <a href="{{route('vendor.dashboard')}}">
                 <div class="parent-icon"><i class='bx bx-cookie'></i>
                 </div>
                 <div class="menu-title">Dashboard</div>
